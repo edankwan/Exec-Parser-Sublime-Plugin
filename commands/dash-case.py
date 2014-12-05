4360a0c83a2001edc662d77e6c3689927a8ec0ca
Edan Kwan <edan@edankwan.com>
2014-12-05 11:41:48 +0000

    add assets

--- commands/dash-case.py
+++
@@ -1,4 +0,0 @@
-_tmp = re.sub(r'([a-z])([A-Z])', r'\1 \2', output)
-_tmp = re.sub(r'[^\w\n\r]+', '_', _tmp.lower())
-_tmp = re.sub(r'_+', '_', _tmp)
-output = re.sub(r'^( *).*', r'\1', output) + re.sub(r'^[^_\w\n\r]*(_*).*', r'\1', output) + _tmp.strip('_').replace('_', '-')
